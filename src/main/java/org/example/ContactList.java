6f4cbf55d24384de6608a7870af55621abfb8224
tringapps-srinivasan <31205587+Srinivasan-TA@users.noreply.github.com>
2023-02-09 12:33:58 +0530

    initial

--- src/main/java/org/example/ContactList.java
+++ src/main/java/org/example/ContactList.java
@@ -1,7 +1,9 @@
 package org.example;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.logging.Logger;
 public class ContactList {
+    static Logger l = Logger.getLogger("com.api.jar");
     static HashMap<String, Contact> contacts = new HashMap<>();
     static class Contact {
         String name;
@@ -17,56 +19,56 @@ public class ContactList {
         Scanner sc = new Scanner(System.in);
         int choice = 0;
         while (choice != 4) {
-            System.out.println("1.add Contact \n2.delete a contact \n3.Search Contact \n4.Exit");
+            l.info("1.add Contact \n2.delete a contact \n3.Search Contact \n4.Exit");
             choice = sc.nextInt();
             sc.nextLine();
             switch (choice) {
                 case 1:
-                    System.out.print("Enter name: ");
+                    l.info("Enter name: ");
                     String name = sc.nextLine();
-                    System.out.print("Enter phone: ");
+                    l.info("Enter phone: ");
                     String phone = sc.nextLine();
-                    System.out.print("Enter email: ");
+                    l.info("Enter email: ");
                     String email = sc.nextLine();
                     addContact(name, phone, email);
                     break;
                 case 2:
-                    System.out.print("Enter name: ");
+                    l.info("Enter name to be display: ");
                     name = sc.nextLine();
                     deleteContact(name);
                     break;
                 case 3:
-                    System.out.print("Enter name: ");
+                    l.info("Enter name to be deleted : ");
                     name = sc.nextLine();
                     searchContact(name);
                     break;
                 case 4:
-                    System.out.println("Exiting...");
+                    l.info("Exiting...");
                     break;
                 default:
-                    System.out.println("Invalid choice, try again.");
+                    l.info("Invalid choice, try again.");
             }
         }
     }
     static void addContact(String name, String phone, String email) {
         contacts.put(name, new Contact(name, phone, email));
-        System.out.println("Contact added successfully.");
+        l.info("Contact added successfully.");
     }
     static void deleteContact(String name) {
         if (contacts.remove(name) != null) {
-            System.out.println("Contact deleted successfully.");
+            l.info("Contact deleted successfully.");
         } else {
-            System.out.println("Contact not found.");
+            l.info("Contact not found.");
         }
     }
     static void searchContact(String name) {
         Contact contact = contacts.get(name);
         if (contact != null) {
-            System.out.println("Name: " + contact.name);
-            System.out.println("Phone: " + contact.phone);
-            System.out.println("Email: " + contact.email);
+            l.info("Name: " + contact.name);
+            l.info("Phone: " + contact.phone);
+            l.info("Email: " + contact.email);
         } else {
-            System.out.println("Contact not found.");
+            l.info("Contact not found.");
         }
     }
 }
